ccf169a2a5e7e0b59e3560f526933622fd21e26e
Andrei Tarassov <tamogenia@gmail.com>
2026-05-18 19:11:14 +0300

    Added Readme

--- src/main/java/com/app/service/gateway/config/AuthenticationFilter.java
+++ src/main/java/com/app/service/gateway/config/AuthenticationFilter.java
@@ -17,7 +17,6 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
